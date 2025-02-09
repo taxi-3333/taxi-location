f02cff0cf1ef7b688337f2b40808dc2ff796a00a
taxi-3333 <oioi.korakora.nanja@gmail.com>
2025-02-09 15:32:00 +0900

    　s

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  define: {
-    "import.meta.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
-    "import.meta.env.VITE_SPREADSHEET_ID": JSON.stringify(process.env.VITE_SPREADSHEET_ID),
-  }
-});
+export default defineConfig({});
